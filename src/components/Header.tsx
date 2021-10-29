87a8437ec027060af69ac150bc938fa636fde29f
Jonathan Geiger <geiger01@Jonathans-MBP.lan>
2021-10-29 20:31:07 +0300

    top header is ready

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,22 +2,30 @@ import React from 'react'
 import logo from '../assets/img/logo.svg'
 
 import { IoSearchOutline } from "react-icons/io5";
+import { IoMdApps } from "react-icons/io";
+import { RiVideoAddLine } from "react-icons/ri";
+import { BsBell } from "react-icons/bs";
+
+
 
 export const Header = () => {
     return (
         <header className="header">
             <div className="logo">
-            <img src={logo} alt="youtube-logo" />
+                <img src={logo} alt="youtube-logo" />
             </div>
             <div className="youtube-search">
-                <input type="text" placeholder="Search"/>
+                <input type="text" placeholder="Search" />
                 <button className="search-icon">
-                <IoSearchOutline />
-                    
+                    <IoSearchOutline />
+
                 </button>
             </div>
             <div className="youtube-actions">
-                ss
+                <RiVideoAddLine />
+                <IoMdApps />
+                <BsBell className="bell-icon" />
+                <div className="user-img"></div>
             </div>
         </header>
     )
